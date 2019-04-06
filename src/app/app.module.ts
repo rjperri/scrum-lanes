c61a998bf4a7488e4d7cb19396ab9a82e068a81e
rjperri <perrirj68@gmail.com>
2019-04-06 06:29:50 -0400

    Added Drag and drop example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import {DemoMaterialModule} from './material-module';
 import { AppComponent } from "./app.component";
 import { TaskBoardComponent } from "./task-board/task-board.component";
 import { TaskService } from "./task.service";
+import {CdkDragDropConnectedSortingExample} from "./drag-and-drop/cdk-drag-drop-connected-sorting-example"
 
 @NgModule({
-  declarations: [AppComponent, TaskBoardComponent],
+  declarations: [AppComponent, TaskBoardComponent, CdkDragDropConnectedSortingExample],
   imports: [BrowserModule, DemoMaterialModule],
   providers: [TaskService],
   bootstrap: [AppComponent]
