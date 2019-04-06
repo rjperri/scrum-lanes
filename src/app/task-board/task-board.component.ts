5d6b551b4c81198cd59754095f8803d2c743443d
rjperri <perrirj68@gmail.com>
2019-04-06 07:12:26 -0400

    Added dragand drop to task board

--- src/app/task-board/task-board.component.ts
+++ src/app/task-board/task-board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Task } from "../task";
 import { TaskService } from "../task.service";
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from 
+'@angular/cdk/drag-drop';
 
 @Component({
   selector: "task-board",
@@ -8,14 +10,26 @@ import { TaskService } from "../task.service";
   styleUrls: ["./task-board.component.css"]
 })
 export class TaskBoardComponent implements OnInit {
-  thing: string;
-  tasks: Task[];
+  todo: Task[];
+  done: Task[];
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit() {
     this.taskService.getTasks().subscribe(tasks => {
-      this.tasks = tasks;
+      this.todo = tasks;
+      this.done = [];
     });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 }
